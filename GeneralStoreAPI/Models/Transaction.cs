5ba6d03e3918dc5dbf2e7560fbcc1e2243fee640
David Lavanchy <dplavanchy@gmail.com>
2021-08-10 19:48:43 -0400

    migrations

--- GeneralStoreAPI/Models/Transaction.cs
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,21 @@ namespace GeneralStoreAPI.Models
 {
     public class Transaction
     {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey("ProductSKU")]
+        public int ProductId { get; set; }
+        public virtual Product ProductSKU { get; set; }
+
+        [Required]
+        public int ItemCount { get; set; }
+
+        [Required]
+        public DateTime DateOfTransaction { get; set; }
     }
 }
