6b8a4e9fe4ec402ab35e6bb2da04a98137ab916b
David Lavanchy <dplavanchy@gmail.com>
2021-08-10 19:06:20 -0400

    scaffolded out models and DbContext class

---
+++ GeneralStoreAPI/Models/GeneralStoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class GeneralStoreDbContext : DbContext
+    {
+        public GeneralStoreDbContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Customer.Customer> Customers { get; set; }
+        public DbSet<Product.Product> Products { get; set; }
+        public DbSet<Transaction.Transaction> Transactions { get; set; }
+    }
+}
